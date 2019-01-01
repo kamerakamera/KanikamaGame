e19864b3005a5045df51de437f1ab82a6554db0b
kamera <asahi20000124@icloud.com>
2019-01-02 02:40:17 +0900

    aporoが3バウンドするように変更、3バウンドしたら消滅し、aporoparticleを生成するように変更

--- Patora'sHarem/Assets/Scripts/3DMode/Bullet.cs
+++ Patora'sHarem/Assets/Scripts/3DMode/Bullet.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 	[SerializeField]private Rigidbody rb;
 	[SerializeField]private int shotpower;
+	[SerializeField]private GameObject hitParticle;
+	private int hitCount;
 	// Use this for initialization
 	void Start () {
+		hitCount = 0;
 		Move();
 	}
 	
@@ -16,7 +19,19 @@ public class Bullet : MonoBehaviour {
 	}
 
 	void Move(){
-		rb.velocity = (transform.forward + new Vector3(0,0.4f,0))* shotpower;
-		//rb.AddForce(transform.forward * shotpower);
+		rb.velocity = (transform.forward + new Vector3(0,0.25f,0))* shotpower;
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if(other.collider.tag == "Enemy"){
+			//敵にDamege処理
+		}
+		if(other.collider.tag != "Player" && other.collider.tag != "Bullet"){
+			hitCount++;
+		}
+		if(hitCount >= 3){
+			Destroy(this.gameObject);
+			Instantiate(hitParticle,transform.position,Quaternion.identity);
+		}
 	}
 }
